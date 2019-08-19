77bfeb4de753c5706dadd91f43aca3c03be48c0c
Mike <mikeglopez.91@gmail.com>
2019-08-19 00:28:27 -0500

    Fix incorrect quote usage

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -7,7 +7,7 @@ import Markers from './Markers.jsx';
 
 const Map = compose(
   withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`,
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: '100%' }} />,
     containerElement: <div style={{ height: '600px' }} />,
     mapElement: <div style={{ height: '100%' }} />,
