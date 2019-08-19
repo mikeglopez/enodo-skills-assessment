d5536c61232f068bf5bfcdc589854d162dee0c7d
Mike <mikeglopez.91@gmail.com>
2019-08-19 01:48:07 -0500

    Add Brotli and GZip compression

---
+++ eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'class-methods-use-this': 'off',
+    'comma-dangle': 'off',
+    'consistent-return': 'off',
+    'import/extensions': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/mouse-events-have-key-events': 'off',
+    'max-len': 'off',
+    'no-console': 'off',
+    'no-undef': 'off',
+    'prefer-destructuring': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-array-index-key': 'off',
+    'react/prop-types': 'off'
+  }
+};
